75d5287b6290f5540f9cee6c7ae117f765b63b38
Ruben Marcelo Baptista Silva <a54949@alunos.ipb.pt>
2026-04-20 20:17:23 +0100

    adicionar modelo tarefas

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from datetime import datetime
 from app.database import Base
 
 class User(Base):
@@ -10,4 +11,14 @@ class User(Base):
     password = Column(String(255), nullable=False)
     is_active = Column(Boolean, default = True)
 
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id          = Column(Integer, primary_key=True, index=True)
+    title       = Column(String(100), nullable=False)
+    description = Column(String(500), nullable=True)
+    completed   = Column(Boolean, default=False)
+    priority    = Column(String(10), default="media")
+    created_at  = Column(DateTime, default=datetime.utcnow)
+    owner_id    = Column(Integer, ForeignKey("users.id"), nullable=False)
 
