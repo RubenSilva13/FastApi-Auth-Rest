fb51271a8df826208c002878d64ae74b16052f58
RubenSilva13 <rubenmbs123@gmail.com>
2026-04-25 15:53:44 +0100

    adicionar CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine
 from app import models
 from app.routers import auth, tasks
@@ -6,6 +7,14 @@ from app.routers import auth, tasks
 models.Base.metadata.create_all(bind=engine, checkfirst=True)
 
 app = FastAPI(title="Minha API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "https://fastapi-auth-react.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(tasks.router)
